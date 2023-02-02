688093afdda320273feefe5513f4d8d59ecb95d0
Henry-Julian <hjqg689@gmail.com>
2023-02-01 20:39:27 -0500

    Solución punto 4

---
+++ todo_Sobre_Variables_01_02_2023/main.js
@@ -0,0 +1,15 @@
+//4.	Haz un programa de nombre main.js y otro llamado index.html 
+//incluir el archivo JavaScript en el <head></ head>,
+// que has de grabar en una carpeta llamada todo_Sobre_Variables_FechaActual, y 
+//declarar variable y asignar a cada una de ellas un valor de tipo String,Number,Booleano y 
+//mostrar los valores por medio de la consola o de la ventana emergente, 
+//Nota la variable de tipo constante const el tipo de valor es opcional.
+
+
+let nombre= "Henry";
+let cédula= 1234567890;
+let masculino= true;
+
+console.log(`El nombre del alumno es ${nombre}`);
+console.log(`La identificación del alumno es ${cédula}`);
+console.log(`El género del alumno es ${masculino}`);
